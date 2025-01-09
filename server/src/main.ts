39f5eab8b72bc939688834107d00e38113422811
hikashi <hikashimlbb@gmail.com>
2025-01-09 15:27:12 +0700

    Changed Express to NestJS

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,19 +2,21 @@ import dotenv from 'dotenv'
 import path from 'path'
 dotenv.config({ path: path.resolve(__dirname, '../', '../', '.env') })
 
-import express from 'express'
+import { NestFactory } from '@nestjs/core';
 import { initializeDatabase } from './config/db';
+import { AppModule } from './app.module';
 
 const PORT: number = Number(process.env.PORT) || 3001;
 
-const app = express();
-
 const startApp = async () => {
     try {
-        await initializeDatabase()
+        await initializeDatabase();
+
+        const app = await NestFactory.create(AppModule);
+        
         app.listen(PORT, () => {
             console.info(`\x1b[34mServer currently listening to port ${PORT}...\x1b[0m`);
-        })
+        });
     } catch (error) {
         console.error(`\x1b[41mAn error occurred while starting the application:\n\x1b[0m\x1b[31m${error}\x1b[0m`);
     }
