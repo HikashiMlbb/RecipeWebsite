4803660e6b7da715c2c6cb02de7dfe8bc0d9102c
hikashi <hikashimlbb@gmail.com>
2025-01-10 17:19:02 +0700

    Added database initialization debug

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -16,24 +16,32 @@ export const initializeDatabase = async () => {
         throw new Error("Unable to connect to the database.");
     }
 
+    console.info("\x1b[34mDatabase is currently initializing...\x1b[0m");
+
     // Creating User
     await pool.query(queries.createUser);
+    console.info("\x1b[34m[*] Users table is successfully initialized!\x1b[0m");
 
     // Creating Recipe
     await pool.query(queries.createRecipe);
+    console.info("\x1b[34m[*] Recipes table is successfully initialized!\x1b[0m");
 
     // Creating Comment
     await pool.query(queries.createComment);
+    console.info("\x1b[34m[*] Comments table is successfully initialized!\x1b[0m");
 
     // Creating RecipeRating
     await pool.query(queries.createRecipeRating);
+    console.info("\x1b[34m[*] RecipeRatings table is successfully initialized!\x1b[0m");
 
     // Creating Ingredient
     await pool.query(queries.createIngredient);
+    console.info("\x1b[34m[*] Ingredients table is successfully initialized!\x1b[0m");
 
     // Creating Trigger with Function on RecipeRating
     await pool.query(queries.createCalculateRecipeRatingFunction);
     await pool.query(queries.createRecipeRatingOnChangeTrigger);
+    console.info("\x1b[34m[*] Trigger on RecipeRating is successfully initialized!\x1b[0m");
 
     console.info("\x1b[34mDatabase is successfully initialized!\x1b[0m");
 }
