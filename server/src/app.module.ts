baf9d00a80ae8df9b0ad848bf33d37e70c0e527c
hikashi <hikashimlbb@gmail.com>
2025-01-12 12:46:15 +0700

    Recipes module initial create

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UsersModule } from './users/users.module';
 import { JwtModule } from "@nestjs/jwt";
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
   imports: [
     UsersModule,
+    RecipesModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET_CODE,
---
+++ server/src/recipes/recipes.controller.ts
@@ -0,0 +1,28 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('recipes')
+export class RecipesController {
+    async create() {
+
+    }
+
+    async getById() {
+
+    }
+
+    async getByAuthor() {
+
+    }
+
+    async getByPagination() {
+
+    }
+
+    async update() {
+
+    }
+
+    async delete() {
+        
+    }
+}
